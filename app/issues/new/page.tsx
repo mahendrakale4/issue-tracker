d5a0fdf855528c72b374d55d04ad39faae1230ae
mahendrakale4 <mkale4843@gmail.com>
2024-11-17 19:42:43 +0530

    refactor: Extract error message display into reusable ErrorMessage component
    - Created a reusable ErrorMessage component
     - readable and DRY

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
 import { createIssueSchema } from "@/app/validationSchema"
+import ErrorMessage from "@/app/components/ErrorMessage"
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -55,11 +56,11 @@ const NewIssuePage = () => {
               value={field.value || ""}></TextField.Root>
           )}
         />
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+        
+          <ErrorMessage>
+            {errors.title?.message}
+          </ErrorMessage>
+        
         {/* Description */}
         <Controller
           name="description"
@@ -68,11 +69,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
+       
+          <ErrorMessage>
+            {errors.description?.message}
+          </ErrorMessage>
+      
 
         {/* Submit */}
         <Button>Submit New Issue</Button>
