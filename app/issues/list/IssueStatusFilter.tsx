322a242f2d234d72dfe887886f452adae8d387f8
Mahendra kale <mkale4843@gmail.com>
2024-12-23 23:02:03 +0530

    feat: Add sorting , filters, NextLink navigation, and Populate 'Issue' table with sample entries
    - Integrated NextLink for dynamic URL handling, combining old and new query parameters
    
    - Added sorting options (ASC, DESC) for dynamic order control.
    
    - Ensured OrderBy is removed after issue status filter is applied.
    
    - Enhanced IssueStatusFilter with useSearchParams() to automatically apply query filters for status and orderBy.
    
    - Added realistic issues with different statuses and descriptions.

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,11 +1,10 @@
 "use client"
 import { Status } from "@prisma/client"
 import { Select } from "@radix-ui/themes"
-import { useRouter } from "next/navigation"
-import React from "react"
+import { useRouter, useSearchParams } from "next/navigation"
 
 const statuses: { label: string; value?: Status }[] = [
-  { label: "All", value: "ALL" },
+  { label: "All" ,value: "All" },
   { label: "Open", value: "OPEN" },
   { label: "In Progress", value: "IN_PROGRESS" },
   { label: "Closed", value: "CLOSED" },
@@ -13,10 +12,17 @@ const statuses: { label: string; value?: Status }[] = [
 
 const IssueStatusFilter = () => {
   const router = useRouter()
+  const searchParams = useSearchParams()
+
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(status) => {
-        const query = status ? `?status=${status}` : ""
+        const params = new URLSearchParams()
+        if (status) params.append("status", status)
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!)
+        const query = params.size ? "?" + params.toString() : ""
         router.push("/issues/list" + query)
       }}>
       <Select.Trigger placeholder="Filter by status..." />
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,26 +1,50 @@
 // 'use client'
-import React from "react"
-import { Table } from "@radix-ui/themes"
 import { IssueStatusBadge, Link } from "@/app/components"
-import IssueAction from "../_components/IssueAction"
 import prisma from "@/prisma/client"
-import { Status } from "@prisma/client"
-interface Props {
-  searchParams: { status: Status }
-}
-const IssuePage = async ({ searchParams }: any) => {
-  const PromisedsearchParams = await searchParams
-  const statuses = Object.values(Status) // ["OPEN", "IN_PROGRESS", "CLOSED"] : undefined
-console.log(PromisedsearchParams.status)
-  const result = statuses.includes(PromisedsearchParams.status)
-    ? PromisedsearchParams.status
-    : undefined
+import { Issue, Status } from "@prisma/client"
+import { CaretSortIcon } from "@radix-ui/react-icons"
+import { Table } from "@radix-ui/themes"
+import NextLink from "next/link"
+import IssueAction from "../_components/IssueAction"
 
-console.log(result)
+
+interface Props {
+  searchParams: { status: Status; orderBy: keyof Issue; order: "asc" | "desc" }
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const qp = await searchParams
+  const columns: {
+    label: string
+    value: keyof Issue
+    className?: string
+  }[] = [
+    { label: "Issue", value: "title" },
+    {
+      label: "Status",
+      value: "status",
+      className: "hidden md:table-cell",
+    },
+    {
+      label: "Created",
+      value: "createdAt",
+      className: "hidden md:table-cell",
+    },
+  ]
+
+  const statuses = Object.values(Status)
+  const status = statuses.includes(qp.status) ? qp.status : undefined
+
+  const currentOrder = qp.order || "asc"
+  const newOrder = currentOrder === "asc" ? "desc" : "asc"
+
+  const orderBy = qp.orderBy ? { [qp.orderBy]: currentOrder } : undefined
+
   const issues = await prisma.issue.findMany({
     where: {
-      status: result,
+      status: status,
     },
+    orderBy,
   })
 
   return (
@@ -29,13 +53,23 @@ console.log(result)
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell> Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}>
+                <NextLink
+                  href={{
+                    query: {
+                      ...qp,
+                      orderBy: column.value,
+                      order: qp.orderBy === column.value ? newOrder : "asc",
+                    },
+                  }}>
+                  {column.label} <CaretSortIcon className={`inline `} />
+                  {qp.orderBy === column.value}
+                </NextLink>
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -61,5 +95,106 @@ console.log(result)
     </div>
   )
 }
+
+
 export const dynamic = "force-dynamic"
-export default IssuePage
+export default IssuesPage
+
+// // 'use client'
+// import React from "react"
+// import { Table } from "@radix-ui/themes"
+// import { IssueStatusBadge, Link } from "@/app/components"
+// import NextLink from "next/link"
+// import IssueAction from "../_components/IssueAction"
+// import prisma from "@/prisma/client"
+// import { Issue, Status } from "@prisma/client"
+// import { ArrowUpIcon } from "@radix-ui/react-icons"
+// interface Props {
+//   searchParams: { status: Status; orderBy: keyof Issue }
+// }
+
+// const IssuesPage = async ({ searchParams }: Props) => {
+//   const columns: {
+//     label: string
+//     value: keyof Issue
+//     className?: string
+//   }[] = [
+//     { label: "Issue", value: "title" },
+//     {
+//       label: "Status",
+//       value: "status",
+//       className: "hidden md:table-cell",
+//     },
+//     {
+//       label: "Created",
+//       value: "createdAt",
+//       className: "hidden md:table-cell",
+//     },
+//   ]
+
+//   const PromisedsearchParams = await searchParams
+//   const statuses = Object.values(Status) // ["OPEN", "IN_PROGRESS", "CLOSED"] : undefined
+//   const result = statuses.includes(PromisedsearchParams.status)
+//     ? PromisedsearchParams.status
+//     : undefined
+
+//   const orderBy = PromisedsearchParams.orderBy
+//     ? { [PromisedsearchParams.orderBy]: "asc" }
+//     : undefined
+
+//   const issues = await prisma.issue.findMany({
+//     where: {
+//       status: result,
+//     },
+//     orderBy: {
+//       [orderBy]: "asc",
+//     },
+//   })
+
+//   return (
+//     <div>
+//       <IssueAction />
+//       <Table.Root variant="surface">
+//         <Table.Header>
+//           <Table.Row>
+//             {columns.map((column) => (
+//               <Table.ColumnHeaderCell key={column.value}>
+//                 <NextLink
+//                   href={{
+//                     query: { ...PromisedsearchParams, orderBy: column.value },
+//                   }}>
+//                   {column.label}
+//                 </NextLink>
+//                 {column.value === PromisedsearchParams.orderBy && (
+//                   <ArrowUpIcon className="inline" />
+//                 )}
+//               </Table.ColumnHeaderCell>
+//             ))}
+//           </Table.Row>
+//         </Table.Header>
+
+//         <Table.Body>
+//           {issues.map((issue) => (
+//             <Table.Row key={issue.id}>
+//               <Table.Cell>
+//                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+//                 <div className="block md:hidden">
+//                   <IssueStatusBadge status={issue.status} />
+//                 </div>
+//               </Table.Cell>
+//               <Table.Cell className=" hidden md:table-cell">
+//                 <IssueStatusBadge status={issue.status} />
+//               </Table.Cell>
+//               <Table.Cell className=" hidden md:table-cell">
+//                 {/* {issue.createdAt.toDateString()} */}
+//                 {/* {new Date(issue.createdAt).toDateString()} */}
+//               </Table.Cell>
+//             </Table.Row>
+//           ))}
+//         </Table.Body>
+//       </Table.Root>
+//     </div>
+//   )
+// }
+// export const dynamic = "force-dynamic"
+// export default IssuesPage
